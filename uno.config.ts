2f36e811f6fa91e0bf4f8ff385d8f5701a5b003b
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-09 15:08:54 +0800

    chore: update Uno.css configuration files
    - Add `postcss.config.cjs` file with `@unocss/postcss` plugin configuration
    - Update `src/style.css`:
      - Remove `@unocss all;` directive
      - Add `font-family` and `line-height` properties to `:root` selector
    - Add `uno.config.ts` file with `unocss` configuration presets
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

---
+++ uno.config.ts
@@ -0,0 +1,28 @@
+// uno.config.ts
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+} from "unocss";
+
+export default defineConfig({
+  presets: [
+    presetUno({
+      attributifyPseudo: true,
+    }),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+    }),
+    presetTypography(),
+    presetWebFonts({
+      provider: "none",
+      fonts: {
+        script: "Homemade Apple",
+      },
+    }),
+  ],
+});
