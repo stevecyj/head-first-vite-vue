f8f6fd3db5c4f31dddae9055233833526d030fb7
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-09 15:09:10 +0800

    feat: add Uno component to App.vue
    - Add `Uno` component to `App.vue`
    - Create new file `src/components/Uno/index.vue`
    - Add script and template sections to `src/components/Uno/index.vue`
    - Add style section to `src/components/Uno/index.vue`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Header from './components/Header/index.vue'
+import Uno from './components/Uno/index.vue'
 </script>
 
 <template>
   <div>
     <Header />
+    <Uno />
     <p class="content">This is Content</p>
   </div>
 </template>
---
+++ src/components/Uno/index.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div
+    class="h-screen flex items-center justify-center text-5xl text-purple fw100 animate-bounce-alt animate-count-infinite animate-duration-1s"
+  >
+    UnoCSS
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  color: $theme-color;
+}
+</style>
