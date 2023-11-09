daaeb8b92840416ace457facc63d1ae5fed4273a
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-09 15:32:04 +0800

    chore: update plugin name and add autoprefixer plugin
    - Change the plugin name from `'@unocss/postcss'` to `"@unocss/postcss"`
    - Add an autoprefixer plugin with specified target browsers
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,11 @@
 // postcss.config.cjs
 module.exports = {
   plugins: {
-    '@unocss/postcss': {},
+    "@unocss/postcss": {},
+    // autoprefixer
+    autoprefixer: {
+      // 指定目标浏览器
+      overrideBrowserslist: ["last 2 versions", "> 1%"],
+    },
   },
-}
+};
