6d1e08690698f17c31ee4fb9092e5dc5ec358c3e
stevecyj <ycsteven.tsao@gmail.com>
2023-11-10 06:41:11 +0800

    chore: update project configurations
    - Update `.eslintrc.cjs`:
      - Add `node: true` to the `env` object
      - Update `extends` array
      - Update `overrides` array
      - Update `parserOptions` object
      - Update `plugins` array
    - Update `tsconfig.json`:
      - Update `include` array
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['standard-with-typescript', 'plugin:vue/vue3-essential'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json', 'tsconfig.node.json'],
+    extraFileExtensions: ['.vue']
+  },
+  plugins: ['vue'],
+  rules: {}
 }
