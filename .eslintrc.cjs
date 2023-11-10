c6b1c7aee9790b95dac5ae0c6c1a863e7b71cab8
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-10 09:18:17 +0800

    chore: update parsers in eslint configuration
    - Update the parser in the .eslintrc.cjs file to "vue-eslint-parser"
    - Change the parserOptions parser to "@typescript-eslint/parser"
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,8 +23,9 @@ module.exports = {
       }
     }
   ],
-  parser: "@typescript-eslint/parser",
+  parser: "vue-eslint-parser",
   parserOptions: {
+    parser: "@typescript-eslint/parser",
     ecmaVersion: "latest",
     sourceType: "module",
     project: ["tsconfig.json", "tsconfig.node.json"],
