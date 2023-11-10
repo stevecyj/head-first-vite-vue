7341592610c5251fae9cb7158704436f2bcc26d6
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-10 10:00:32 +0800

    feat: refactor Vite configuration for ESLint integration
    - Add `import { normalizePath } from "vite";` to the file
    - Add `import viteEslint from "vite-plugin-eslint";` to the file
    - Add `viteEslint()` to the `plugins` array
    - Remove `import { normalizePath } from "vite";` from the file
    - Remove `additionalData: `@import "${variablePath}";``, ` from the `scss` object in the `preprocessorOptions` object
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, normalizePath } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { normalizePath } from "vite";
 import path from "path";
+import viteEslint from "vite-plugin-eslint";
 
 const variablePath = normalizePath(path.resolve("./src/variables.scss"));
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteEslint()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "${variablePath}";`,
-      },
-    },
-  },
+        additionalData: `@import "${variablePath}";`
+      }
+    }
+  }
 });
