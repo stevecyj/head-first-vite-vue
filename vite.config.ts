1caa0bd9585f2df2ef1f89083f74294217a6a488
stevecyj <ycsteven.tsao@gmail.com>
2023-11-11 17:38:13 +0800

    chore: configure stylelint plugin in vite.config.ts
    - Added `vite-plugin-stylelint` as a plugin in the `vite.config.ts` file
    - Excluded `node_modules` directory from stylelint in `vite.config.ts` file
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import { defineConfig, normalizePath } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import viteEslint from "vite-plugin-eslint";
+import viteStylelint from "vite-plugin-stylelint";
 
 const variablePath = normalizePath(path.resolve("./src/variables.scss"));
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteEslint()],
+  plugins: [
+    vue(),
+    viteEslint(),
+    viteStylelint({
+      exclude: ["node_modules/**"]
+    })
+  ],
   css: {
     preprocessorOptions: {
       scss: {
